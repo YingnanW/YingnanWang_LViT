1a3c21af148a91804e6a514478b66daa117f9635
yingnanWang <109457477+YingnanW@users.noreply.github.com>
2025-10-14 11:05:05 +0800

    Add files via upload

---
+++ LVIT/code/arguments.py
@@ -0,0 +1,29 @@
+import argparse
+def args_parser():
+    parser.add_argument('--dataset', type=str, default='IM', choices=['IM','GA'], help="name of dataset")
+    parser.add_argument('--model', type=str, default='double_ILViT_GFM2', help='model name')
+
+    parser.add_argument('--lr', type=float, default=0.01, help='learning rate')
+    parser.add_argument('--momentum', type=float, default=0.9, help='SGD momentum (0.5~0.9)')
+    parser.add_argument('--weight_decay', type=float, default=5e-4)
+    # parser.add_argument('--milestones', type=float, default=[100], help='Attenuation steps in SGD')
+    parser.add_argument('--gamma', type=float, default=0.8, help='Attenuation factor in SGD')
+    parser.add_argument('--optim', type=str, default='sgd')
+    parser.add_argument('--stepsize', type=int, default=10)
+    
+    parser.add_argument('--b', type=float, default=0.3)  
+    #策略选择
+    parser.add_argument('--pair', type=str, default='A' ,choices=['A','A+B'])
+    parser.add_argument('--output', type=str, default='more' ,choices=['main','more'])
+ #   parser.add_argument('--fushion', type=str, default='mix',choices=['mix','finall'])
+    parser.add_argument('--epochs', type=int, default=150, help='the number of epochs')
+    parser.add_argument('--batchsize', type=int, default=24, help='the size of batch')
+    parser.add_argument('--num_classes', type=int, default=2, help="number of classes")
+    parser.add_argument('--gpu', type=int,default=1, help="To use cuda, set to a specific GPU ID. Default set to use CPU.")
+    parser.add_argument('--ngpu', type=bool, default=False, help='use n gpu')
+    parser.add_argument('--stopping_rounds', type=int, default=10, help='rounds of early stopping')
+    parser.add_argument('--seed', type=int, default=42, help='random seed')
+
+    args = parser.parse_args()
+    return args
+args = args_parser()
---
+++ LVIT/code/datasets.py
@@ -0,0 +1,227 @@
+import torch.utils.data as data
+import torch
+import numpy as np
+from PIL import Image
+import torch.backends.cudnn as cudnn
+from torchvision.transforms.functional import InterpolationMode
+import os
+import glob2# 
+from torchvision import transforms
+import random
+from torchvision.transforms.functional import InterpolationMode
+from arguments import args
+
+def set_random_seed(seed_value):
+    torch.manual_seed(seed_value)
+    torch.cuda.manual_seed(seed_value)
+    torch.cuda.manual_seed_all(seed_value)
+    random.seed(seed_value)
+    np.random.seed(seed_value)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+set_random_seed(42)
+
+ 
+  
+wli_dir = ['WLI/IM/', 'WLI/GA/', 'WLI/Normal/']
+lci_dir = ['LCI/IM/', 'LCI/GA/', 'LCI/Normal/']   
+    
+
+wli_dir_list0 = ['WLI/IM/贲门/','WLI/IM/胃底/','WLI/IM/胃窦/','WLI/IM/胃角/','WLI/IM/胃体/',
+                'WLI/GA/贲门/','WLI/GA/胃底/','WLI/GA/胃窦/','WLI/GA/胃角/','WLI/GA/胃体/',
+                'WLI/Normal/贲门/','WLI/Normal/胃底/','WLI/Normal/胃窦/','WLI/Normal/胃角/','WLI/Normal/胃体/']
+lci_dir_list1 = ['LCI/IM/贲门/','LCI/IM/胃底/','LCI/IM/胃窦/','LCI/IM/胃角/','LCI/IM/胃体/',
+                'LCI/GA/贲门/','LCI/GA/胃底/','LCI/GA/胃窦/','LCI/GA/胃角/','LCI/GA/胃体/',
+                'LCI/Normal/贲门/','LCI/Normal/胃底/','LCI/Normal/胃窦/','LCI/Normal/胃角/','LCI/Normal/胃体/']
+
+data_list_train = []
+data_list_train_B = [] 
+data_list_val = []
+data_list_len = [35,14,1183,600,599,  #2431
+                 77,78,1482,524,1036,  #3197
+                 534,631,740,448,1564]  #3917
+data_all_len = [2431,3197,3917] 
+wli_item_B = []
+lci_item_B = []
+
+pair_count = [int(len_ * 0.2) for len_ in data_all_len]
+        
+for i in range(len(wli_dir_list0)):
+    wli_dir = wli_dir_list0[i]
+    lci_dir = lci_dir_list1[i]
+
+    wli_data_list =  glob2.glob('/home/mch/data/huaxi_data/'+ wli_dir+'*')
+    lci_data_list =  glob2.glob('/home/mch/data/huaxi_data/'+ lci_dir+'*')
+    random.shuffle(wli_data_list)
+    random.shuffle(lci_data_list)
+
+    wli_item = wli_data_list[:int(data_list_len[i]*0.8)]
+    wli_item_val =  wli_data_list[-int(data_list_len[i]*0.2):]
+   
+    lci_item = lci_data_list[:int(data_list_len[i]*0.8)]
+    lci_item_val =  lci_data_list[-int(data_list_len[i]*0.2):]
+
+    item = [f'{wli}、{lci}' for wli, lci in zip(wli_item, lci_item)]
+    item_val = [f'{wli}、{lci}' for wli, lci in zip(wli_item_val, lci_item_val)]
+    
+
+    if args.pair == 'A+B':
+        
+       wli_item_B += wli_data_list[:int(data_list_len[i])]
+       lci_item_B += lci_data_list[:int(data_list_len[i])]
+       if (i+1)%5 == 0:
+            random.shuffle(wli_item_B)
+            random.shuffle(lci_item_B)
+
+            wli_pair = wli_item_B[:pair_count[i//5]]
+            lci_pair = lci_item_B[:pair_count[i//5]]
+            item += [f'{wli}、{lci}' for wli, lci in zip(wli_pair, lci_pair)]
+            wli_item_B = []  
+            lci_item_B = []
+           
+           
+    data_list_train.append(item)
+    data_list_val.append(item_val)
+    
+   
+
+
+data_list0_train = sum(data_list_train[0:5],[])
+data_list1_train = sum(data_list_train[5:10],[])
+data_list2_train = sum(data_list_train[10:15],[])
+
+
+data_list0_val = sum(data_list_val[0:5],[])
+data_list1_val = sum(data_list_val[5:10],[])
+data_list2_val = sum(data_list_val[10:15],[])
+
+
+
+if args.dataset == 'IM':
+    li = [0,2]
+    print('Question:IM')
+    print('data_number:',len(data_list0_train),len(data_list2_train))
+elif args.dataset == 'GA':
+    li = [1,2]
+    print('Question:GA')
+    print('data_number:',len(data_list1_train),len(data_list2_train))
+
+data_name_train = [data_list0_train,data_list1_train,data_list2_train]
+data_name_val = [data_list0_val,data_list1_val,data_list2_val]
+
+train_list0 = data_name_train[li[0]]
+train_list1 = data_name_train[li[1]]
+test_list0 = data_name_val[li[0]]
+test_list1 = data_name_val[li[1]]
+train_list, test_list={},{}
+
+for item in train_list0:
+    train_list[item]=0  
+for item in train_list1:
+    train_list[item]=1
+for item in test_list0:
+    test_list[item]=0
+for item in test_list1:
+    test_list[item]=1 
+random_train, random_test=[], []
+for key, values in train_list.items():
+        random_train.append(key+","+str(values))
+random.shuffle(random_train)
+for key, values in test_list.items():
+        random_test.append(key+","+str(values))
+random.shuffle(random_test)
+if args.pair == 'A+B':
+    B = '2'
+else:
+    B = ''
+with open(f'/home/mch/our_model/Gate_LIVIT/{args.dataset}-train{B}.txt', 'w') as f:
+    for i in random_train:
+        f.write(i+"\n")
+with open(f'/home/mch/our_model/Gate_LIVIT/{args.dataset}-val{B}.txt', 'w') as f:
+    for i in random_test:
+        f.write(i+"\n")
+
+
+def default_loader(path):
+    try:   
+        img = Image.open(path)
+        return img
+    except:
+        print("Cannot read image: {}".format(path))
+
+
+
+data_transforms={
+            'train':transforms.Compose([
+                transforms.RandomChoice([
+                    transforms.Compose([transforms.RandomHorizontalFlip(p=0.5),transforms.RandomVerticalFlip(p=0.5)]),          
+                    transforms.RandomRotation(degrees=180),           # 
+                    transforms.RandomAffine(degrees=0, translate=(0.2, 0.2), 
+                                    scale=None, shear=None, interpolation=InterpolationMode.BILINEAR, fill=0), # 
+                    transforms.Resize(1000)                  
+                                                ]),
+                    transforms.Resize((256,256)),
+                    transforms.CenterCrop(224),
+                    transforms.ToTensor(),
+                    transforms.Normalize([0.4931,0.3016,0.2211],[0.3398,0.215,0.1612]) 
+                ]),
+
+            'val':transforms.Compose([
+                    transforms.Resize((256,256)),
+                    transforms.CenterCrop(224),        
+                    transforms.ToTensor(),
+                    transforms.Normalize([0.4931,0.3016,0.2211],[0.3398,0.215,0.1612])
+                ])
+        }
+
+
+
+
+class customData(Dataset):# 
+    def __init__(self,  txt_path, dataset = '', data_trans1=None, data_trans2=None, loader = default_loader):
+        with open(txt_path) as input_file:
+      
+            lines = input_file.readlines()
+            self.img_name_x1 = [(line.strip().split('、')[0]) for line in lines]
+            self.img_name_x2 = [(line.strip().split('、')[1].split(',')[0]) for line in lines]
+            
+            self.img_label = [int(line.strip().split(',')[-1]) for line in lines]
+        self.trans1 = data_trans1
+        self.trans2 = data_trans2
+        self.dataset = dataset
+        self.loader = loader
+
+    def __len__(self):
+        return len( self.img_label )
+
+    def __getitem__(self, item):
+        label = self.img_label[item]
+        img_name_x1 = self.img_name_x1[item]
+        img_name_x2 = self.img_name_x2[item]
+      
+        img1 = self.loader(img_name_x1)
+        img2 = self.loader(img_name_x2)
+        if self.dataset == 'train':
+            img01 = self.trans1(img1)
+            img02 = self.trans1(img2)
+ 
+                
+        if self.dataset == 'val':
+            img01 = self.trans2(img1) 
+            img02 = self.trans2(img2)
+          
+   
+        img_ = [img01,img02]
+ 
+        return img_, label
+
+image_datasets = {x: customData(txt_path=(f'/home/our_model/Gate_LIVIT/{args.dataset}'+'-' + x +B + '.txt'),
+                                    data_trans1 = data_transforms['train'],
+                                    data_trans2 = data_transforms['val'],          
+                                    dataset= x ) for x in ['train', 'val']}
+dataloders = {x: torch.utils.data.DataLoader(image_datasets[x],
+                                                 batch_size=args.batchsize,
+                                                 num_workers=4,
+                                                 shuffle = False,drop_last=True) for x in ['train','val']}
+
